30603c2f651eac1e2b6df0a66c7ee1f03e0c97dd
UDAnton <uadjob@gmail.com>
2018-12-13 14:13:34 +0200

    Adding info to owner and vet entities, creating speciality entity

--- pet-clinic-data/src/main/java/com/github/udanton/demopetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/github/udanton/demopetclinic/model/Owner.java
@@ -3,8 +3,34 @@ package com.github.udanton.demopetclinic.model;
 import java.util.Set;
 
 public class Owner extends Person {
-
+    private String city;
+    private String adress;
+    private String telephone;
     private Set<Pet> pets;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
 
     public Set<Pet> getPets() {
         return pets;
