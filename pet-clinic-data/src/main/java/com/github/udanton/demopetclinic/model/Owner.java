4bcb867cc53197167757f438f2f6421ad94d8723
udanton <uadjob@gmail.com>
2018-12-18 16:58:54 +0200

    Adding JPA to model

--- pet-clinic-data/src/main/java/com/github/udanton/demopetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/github/udanton/demopetclinic/model/Owner.java
@@ -1,12 +1,23 @@
 package com.github.udanton.demopetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
+
+    @Column(name = "city")
     private String city;
-    private String adress;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
     public String getCity() {
@@ -17,12 +28,12 @@ public class Owner extends Person {
         this.city = city;
     }
 
-    public String getAdress() {
-        return adress;
+    public String getAddress() {
+        return address;
     }
 
-    public void setAdress(String adress) {
-        this.adress = adress;
+    public void setAddress(String adress) {
+        this.address = address;
     }
 
     public String getTelephone() {
