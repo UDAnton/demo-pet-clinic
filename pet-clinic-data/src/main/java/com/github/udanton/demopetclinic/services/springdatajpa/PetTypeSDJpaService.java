15fe7d7438e64760792c08ee22106751833cff14
udanton <uadjob@gmail.com>
2018-12-19 10:38:00 +0200

    Creating pet, vet, speciality, pet type services

--- pet-clinic-data/src/main/java/com/github/udanton/demopetclinic/services/springdatajpa/PetTypeSDJpaService.java
+++ pet-clinic-data/src/main/java/com/github/udanton/demopetclinic/services/springdatajpa/PetTypeSDJpaService.java
@@ -1,4 +1,50 @@
 package com.github.udanton.demopetclinic.services.springdatajpa;
 
-public class PetTypeSDJpaService {
+import com.github.udanton.demopetclinic.model.PetType;
+import com.github.udanton.demopetclinic.repositories.PetTypeRepository;
+import com.github.udanton.demopetclinic.services.PetTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class PetTypeSDJpaService implements PetTypeService {
+
+    private final PetTypeRepository petTypeRepository;
+
+    @Autowired
+    public PetTypeSDJpaService(PetTypeRepository petTypeRepository) {
+        this.petTypeRepository = petTypeRepository;
+    }
+
+    @Override
+    public Set<PetType> findAll() {
+        Set<PetType> petTypes = new HashSet<>();
+        petTypeRepository.findAll().forEach(petTypes::add);
+        return petTypes;
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return petTypeRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return petTypeRepository.save(petType);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        petTypeRepository.delete(petType);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        petTypeRepository.deleteById(id);
+    }
 }
