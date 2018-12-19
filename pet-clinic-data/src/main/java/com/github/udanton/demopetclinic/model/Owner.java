b2d7a7a8bb61905d0ee3d06d1ef1e151c082a9ed
UDAnton <uadjob@gmail.com>
2018-12-19 14:53:52 +0200

    Refactoring models with project lombok
    Refactoring models with project lombok

--- pet-clinic-data/src/main/java/com/github/udanton/demopetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/github/udanton/demopetclinic/model/Owner.java
@@ -1,12 +1,27 @@
 package com.github.udanton.demopetclinic.model;
 
+import lombok.*;
+
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
+
+    @Builder
+    public Owner(Long id, String firstName, String lastName, String city, String address,
+                 String telephone, Set<Pet> pets) {
+        super(id, firstName, lastName);
+        this.city = city;
+        this.address = address;
+        this.telephone = telephone;
+        this.pets = pets;
+    }
 
     @Column(name = "city")
     private String city;
@@ -20,35 +35,4 @@ public class Owner extends Person {
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String adress) {
-        this.address = address;
-    }
-
-    public String getTelephone() {
-        return telephone;
-    }
-
-    public void setTelephone(String telephone) {
-        this.telephone = telephone;
-    }
-
-    public Set<Pet> getPets() {
-        return pets;
-    }
-
-    public void setPets(Set<Pet> pets) {
-        this.pets = pets;
-    }
 }
