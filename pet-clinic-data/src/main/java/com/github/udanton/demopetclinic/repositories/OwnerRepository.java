9b157cc66e58fdb1400c88efd34f86fead6236dc
UDAnton <uadjob@gmail.com>
2019-01-02 12:10:20 +0200

    Find Owner

--- pet-clinic-data/src/main/java/com/github/udanton/demopetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/github/udanton/demopetclinic/repositories/OwnerRepository.java
@@ -3,6 +3,10 @@ package com.github.udanton.demopetclinic.repositories;
 import com.github.udanton.demopetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 }
--- pet-clinic-data/src/main/java/com/github/udanton/demopetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/github/udanton/demopetclinic/services/map/OwnerServiceMap.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -77,5 +78,10 @@ public class OwnerServiceMap extends AbstractMapServices<Owner, Long> implements
                 .filter(owner -> owner.getLastName().equalsIgnoreCase(lastName))
                 .findFirst()
                 .orElse(null);
+    }
+
+    @Override
+    public List<Owner> findAllByLastNameLike(String lastName) {
+        return null;
     }
 }
